671fd7db785cb74f3d42a536ab36f1072cd42e65
IceyWu <3128006406@qq.com>
2024-11-29 17:19:53 +0800

    feat: 添加字体加载状态处理和纸张背景样式

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,10 +26,16 @@ function handleEdit(data) {
 		openDlg()
 	}
 }
+const fontLoading = ref(true)
+// 添加字体加载是否成功的事件
+document.fonts.ready.then(() => {
+  // 使用该字体渲染文字（如：在 canvas 中绘制）
+	fontLoading.value = false
+})
 </script>
 
 <template>
-	<div class="h-[100vh] w-full">
+	<div class="paper-bg h-[100vh] w-full">
 		<div ref="el" class="z-9999" :style="style" style="position: fixed">
 			<button
 				class="flex select-none items-center gap-3 rounded-full bg-white p-3.5 text-center align-middle text-sm text-blue-gray-900 font-bold font-sans uppercase shadow-blue-gray-500/10 shadow-xl transition-all disabled:pointer-events-none active:opacity-[0.85] disabled:opacity-50 focus:opacity-[0.85] active:shadow-none disabled:shadow-none focus:shadow-none hover:shadow-blue-gray-500/20 hover:shadow-lg"
@@ -51,12 +57,21 @@ function handleEdit(data) {
 				<AddFrom :choose-id @dlg-close="closeDlg" />
 			</div>
 		</dialog>
-		<TextList ref="textListRef" msg="minutes" @edit="handleEdit" />
+		<template v-if="fontLoading">
+			<div>
+				loading...
+			</div>
+</template>
+		<TextList v-else ref="textListRef" msg="minutes" @edit="handleEdit" />
 	</div>
 </template>
 
 <style>
 ::-webkit-scrollbar {
   display: none;
+}
+/* 模拟纸张背景 */
+.paper-bg {
+  /* background: #c2c2c2; */
 }
 </style>
